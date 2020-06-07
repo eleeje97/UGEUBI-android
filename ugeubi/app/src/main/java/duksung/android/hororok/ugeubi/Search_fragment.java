94d5d6c6c6f82aa9b7edb67ae51ff77222f55563
unknown <shyunju7@naver.com>
2020-06-07 21:56:39 +0900

    add tab & fragment

---
+++ ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/Search_fragment.java
@@ -0,0 +1,21 @@
+package duksung.android.hororok.ugeubi;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Search_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_search, container, false);
+
+        return rootView;
+    }
+}
