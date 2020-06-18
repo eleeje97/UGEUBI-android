06b34cda9dfe06c92e93d94a077f99f1fb46c970
daeunLee <eleeje97@gmail.com>
2020-06-18 21:48:37 +0900

    activity_add_medicine.xml

--- ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/TestActivity.java
+++ ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/TestActivity.java
@@ -7,6 +7,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_password);
+        setContentView(R.layout.activity_add_medicine);
     }
 }
