f11d8c9ba446336f8b710fce45b80cacd7bfaf35
unknown <shyunju7@naver.com>
2020-06-18 20:57:35 +0900

    add find pw result

---
+++ ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/Find_pw_result.java
@@ -0,0 +1,15 @@
+package duksung.android.hororok.ugeubi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Find_pw_result extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password_result);
+    }
+}
