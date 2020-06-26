7f2149fcc65d34b63ba8abcdbab1538ab8b42cea
daeunLee <eleeje97@gmail.com>
2020-06-26 19:48:20 +0900

    fcm init

--- ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/fcm/MyFirebaseMessagingService.java
+++ ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/fcm/MyFirebaseMessagingService.java
@@ -26,17 +26,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
         // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
+        // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
-            if (true) {
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                //scheduleJob();
             } else {
+                // Handle message within 10 seconds
                 handleNow();
             }
+
         }
 
+
+        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
