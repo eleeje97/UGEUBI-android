3ee49fb166c73689ed47f6dbb8b40c89c779ff10
daeunLee <eleeje97@gmail.com>
2020-06-30 16:56:53 +0900

    약정보검색 api 인터페이스 추가

--- ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/retrofit/RetrofitInterface.java
+++ ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/retrofit/RetrofitInterface.java
@@ -7,6 +7,7 @@ import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -54,5 +55,74 @@ public interface RetrofitInterface {
             "Transfer-Encoding: chunked"})
     @POST("users/sign-in")
     Call<LoginResultDTO> login(@Body LoginDTO loginDTO);
+
+
+
+
+    /** 약/DUR 정보 검색 **/
+
+    // 선택안함
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getDurPrdlstInfoList")
+    Call<DURInfoSearchDTO> getDurPrdlstInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 병용금기
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getUsjntTabooInfoList")
+    Call<DURInfoSearchDTO> getUsjntTabooInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 특정연령대금기
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getSpcifyAgrdeTabooInfoList")
+    Call<DURInfoSearchDTO> getSpcifyAgrdeTabooInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 임부금기
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getPwnmTabooInfoList")
+    Call<DURInfoSearchDTO> getPwnmTabooInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 용량주의
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getCpctyAtentInfoList")
+    Call<DURInfoSearchDTO> getCpctyAtentInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 투여기간주의
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getMdctnPdAtentInfoList")
+    Call<DURInfoSearchDTO> getMdctnPdAtentInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 노인주의
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getOdsnAtentInfoList")
+    Call<DURInfoSearchDTO> getOdsnAtentInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 효능군중복
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getEfcyDplctInfoList")
+    Call<DURInfoSearchDTO> getEfcyDplctInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
+    // 서방형제제 분할주의
+    @Headers({
+            "Content-Type: application/json;charset=UTF-8" ,
+            "Transfer-Encoding: chunked"})
+    @POST("medicine/DURPrdlstInfoService/getSeobangjeongPartitnAtentInfoList")
+    Call<DURInfoSearchDTO> getSeobangjeongPartitnAtentInfoList(@Header("Authorization") String token, @Body DURInfoSearchDTO durInfoSearchDTO);
+
 
 }
