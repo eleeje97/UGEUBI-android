15e37820be38120c69f0ac98b9182e99246e78ba
daeunLee <eleeje97@gmail.com>
2020-07-06 17:21:59 +0900

    DeseData 삭제

--- ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/ugeubi/DoseData.java
+++
@@ -1,8 +0,0 @@
-package duksung.android.hororok.ugeubi.ugeubi;
-
-public class DoseData {
-    public String time;
-    public String dose_name;
-    public String dose_num;
-    public boolean isTaken;
-}
