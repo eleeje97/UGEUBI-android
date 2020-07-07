acb3b02d066806312ff7a9b6c40f4c7f2c3ff4d8
daeunLee <eleeje97@gmail.com>
2020-07-07 10:34:06 +0900

    오픈소스 라이선스

--- ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/PagerAdapter.java
+++ ugeubi/app/src/main/java/duksung/android/hororok/ugeubi/PagerAdapter.java
@@ -34,46 +34,22 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
         switch (position) {
             case 0:
                 Alarm_fragment alarm_fragment = new Alarm_fragment();
-//                setIconUnclicked();
-//                Log.e("TabPosition", tabs.getSelectedTabPosition()+"");
-//                tabs.getTabAt(tabs.getSelectedTabPosition()).setIcon(R.drawable.reminder_icon_clicked);
                 return alarm_fragment;
             case 1:
                 Medicine_kit_fragment medicine_kit_fragment = new Medicine_kit_fragment();
-//                setIconUnclicked();
-//                Log.e("TabPosition", tabs.getSelectedTabPosition()+"");
-//                tabs.getTabAt(tabs.getSelectedTabPosition()).setIcon(R.drawable.medicine_kit_icon_clicked);
                 return medicine_kit_fragment;
             case 2:
                 Ugeubi_fragment ugeubi_fragment = new Ugeubi_fragment();
-//                setIconUnclicked();
-//                Log.e("TabPosition", tabs.getSelectedTabPosition()+"");
-//                tabs.getTabAt(tabs.getSelectedTabPosition()).setIcon(R.drawable.main_icon_clicked);
                 return ugeubi_fragment;
             case 3:
                 Search_fragment search_fragment = new Search_fragment();
-//                setIconUnclicked();
-//                Log.e("TabPosition", tabs.getSelectedTabPosition()+"");
-//                tabs.getTabAt(tabs.getSelectedTabPosition()).setIcon(R.drawable.search_icon_clicked);
                 return search_fragment;
             case 4:
                 Setting_fragment setting_fragment = new Setting_fragment();
-//                setIconUnclicked();
-//                Log.e("TabPosition", tabs.getSelectedTabPosition()+"");
-//                tabs.getTabAt(tabs.getSelectedTabPosition()).setIcon(R.drawable.settings_icon_clicked);
                 return setting_fragment;
             default:
                 return null;
         }
-    }
-
-
-    public void setIconUnclicked() {
-        tabs.getTabAt(0).setIcon(R.drawable.reminder);
-        tabs.getTabAt(1).setIcon(R.drawable.medicine_kit_icon);
-        tabs.getTabAt(2).setIcon(R.drawable.main_icon);
-        tabs.getTabAt(3).setIcon(R.drawable.search_icon);
-        tabs.getTabAt(4).setIcon(R.drawable.settings);
     }
 
     @Override
